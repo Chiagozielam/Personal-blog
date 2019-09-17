24f633635c1d1af6efda5bdd61706601f058bc40
Daniel Don <dandons25@gmail.com>
2019-09-17 13:14:26 +0100

    make the contact form better and connect it with formspree.io

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,28 +4,46 @@ import { Form, Button } from "react-bootstrap"
 const Contact = () => (
   <div>
     <PrimaryLayout>
-      <div className="">
-        <h1>Contact Us</h1>
-        <div className="pt-5">
-          <form action="https://formspree.io/email@domain.tld" method="POST" >
-            <Form classname="pt-5">
-              <Form.Group controlId=" contactForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control name="email" type="email" placeholder="name@example.com" />
-              </Form.Group>
-              <Form.Group controlId=" contactForm.ControlInput1">
-                <Form.Label>Subject</Form.Label>
-                <Form.Control name="subject" type="text" placeholder="Subject..." />
-              </Form.Group>
-              <Form.Group controlId=" contactForm.ControlTextarea1">
-                <Form.Label>Message</Form.Label>
-                <Form.Control nane="message" as="textarea" rows="3" />
-              </Form.Group>
-              <Form.Group controlId=" contactForm.Submit">
-                <Button>Submit</Button>
-              </Form.Group>
-            </Form>
-          </form>
+      <div className="pb-5">
+        <h2 className="text-white" style={{ textAlign: "center", backgroundColor: "#800000", width: "70%", margin: "0 auto", borderRadius: "20px", height: "50px" }}>
+          CONTACT ME
+        </h2>
+        <div className="pt-5" style={{ margin: "0 auto" }}>
+          <div style= {{ width: "400px", margin:" 0 auto"}}>
+            <form action="https://formspree.io/email@domain.tld" method="POST">
+              <p>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="fullname"
+                  id="full-name"
+                  placeholder="Full Name..."
+                  style={{height: "50px"}}
+                />
+              </p>
+              <p>
+                <input
+                  className="form-control"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email Address..."
+                  style={{height: "50px"}}
+                />
+              </p>
+              <textarea
+                class="form-control"
+                id="message"
+                name="message"
+                rows="5"
+                placeholder="Tell Me something Interesting..."
+              ></textarea>
+              <br />
+              <button type="submit" class="btn btn-primary" style={{ width: "300px", height: "50px", alignContent: "center"}}>
+                SEND
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </PrimaryLayout>
