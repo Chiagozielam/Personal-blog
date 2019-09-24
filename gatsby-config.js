53ec5af6b380122196ad115214846c8de686a3f8
Daniel Don <dandons25@gmail.com>
2019-09-24 08:40:09 +0100

    change favicon to a more personalized icon and remove redundant image

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,9 @@ module.exports = {
     description: "This is a blog that gives you any latest content from me",
     keywords: "gatsby, reactjs, graphql, javascript, programming, concepts",
     image: "https://res.cloudinary.com/dcft8yhab/image/upload/v1569179760/IMG_20190922_130327_5_1569178054293.jpg",
-    url: "https://www.spillili.netlify.com",
-    twitterHandle: "@spillcode"
+    url: "https://danieldon.tech",
+    twitterHandle: "@spillcode",
+    icon: "https://favicon-generator.org/favicon-generator/htdocs/favicons/2015-01-13/d8a4c4a5b1e1b908b47010382c1e4438.ico",
   },
   plugins: [
     {
