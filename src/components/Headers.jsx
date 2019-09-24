e35f373ed53b982d6693712317f7af7612a6e5ab
Daniel Don <dandons25@gmail.com>
2019-09-24 09:17:41 +0100

    entirely change navbar temporarily to spot error

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -5,21 +5,27 @@ import {Link} from 'gatsby'
 export default class Headers extends Component {
   render() {
     return (
-      <Navbar bg="light" expand="lg" style={{position: "fixed", zIndex: "10000"}}>
-        <Navbar.Brand href="/"><h3>SpillCode</h3></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto" style={{fontSize: "22px", fontWeight: "500",}}>
-            <Nav.Link>
-              <Link style={{textDecoration: "none"}} to="/">Blog</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link style={{textDecoration: "none"}} to="/contact">Contact Me</Link>
-            </Nav.Link>
-          </Nav>
-          
-        </Navbar.Collapse>
-      </Navbar>
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
     )
   }
 }
